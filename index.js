5c92400987767d59ee61cf717e3d6d4a08141587
Brian Sneddon <bsneddon@gc1.com>
2016-05-03 02:22:26 -0400

    Added getOrderInfo function to generate order info across NiceHash locations and to support multiple orders in each location (currently only for order query, not manipulation).  Refactored raise/lower functions to use getOrderInfo.

--- index.js
+++ index.js
@@ -38,91 +38,124 @@ function httpsRequest(url,callback) {
     });
   });
 }
-function doGetOrders(intent,callback) {
-	console.log("Got to doGetOrders");
-//  var request_url = api_url+"orders.get&my&id="+api_id+"&key="+api_key+"&location=0&algo=1";
-  async.waterfall([
-    function(next) {
-      var request_url = api_url+"orders.get&my&id="+api_id+"&key="+api_key+"&location=0&algo=1";
-      httpsRequest(request_url,next);
-    }],
-    function(err,result) {
-      if (err) {
-        console.log("Error: "+err, err.stack);
-      } else {
-	console.log("Result1: "+result);
-        var replySpeech = "Your order "+result.result.orders[0].id+"is running at "+Number(result.result.orders[0].accepted_speed / 1000).toFixed(2) +" terahashes per second with "+result.result.orders[0].workers+" workers.  Your cost per terahash per day is "+result.result.orders[0].price+" bitcoins.";
-        callback(replySpeech,true);
-      }
-    });
+
+function getOrderInfo(callback) {
+var orders = [];
+
+async.waterfall([
+  function(next) {
+    var request_url = api_url+"orders.get&my&id="+api_id+"&key="+api_key+"&location=0&algo=1";
+    httpsRequest(request_url,function(err,result) {
+    				for (var order of result.result.orders) {
+				  order.location = 0;
+				  orders.push(order);
+				}
+				next(null);
+    				});
+  },
+  function(next) {
+    var request_url = api_url+"orders.get&my&id="+api_id+"&key="+api_key+"&location=1&algo=1";
+    httpsRequest(request_url,function(err,result) {
+                                for (var order of result.result.orders) {
+				  order.location = 1;
+                                  orders.push(order);
+                                }
+                                next(null);
+                                });
+  }],
+  function(err) {
+    if (err) {
+      console.log("Error in new getOrderInfo: "+err);
+      callback(null);
+    } else {
+      console.log("Returning order info: "+orders);
+      callback(null,orders);
+    }
+  });
+
 }
 
+function doGetOrders(intent,callback) {
+	console.log("Got to doGetOrders");
+  var replySpeech = "";
+
+  getOrderInfo(function(err,orders) {
+    if (err) {
+      replySpeech = "There was a system error getting your order info.";
+      callback(replySpeech,true);
+    }
+    console.log("Orders: "+JSON.stringify(orders));
+    for (var order of orders) {
+      replySpeech += "Your order "+order.id+"is running at "+Number(order.accepted_speed / 1000).toFixed(2) +" terahashes per second with "+order.workers+" workers.  Your cost per terahash per day is "+order.price+" bitcoins. ";
+    }
+    callback(replySpeech,true);
+  });
+} 
+    
+
 function doLowerPrice(intent,callback) {
-  var order_id="";
-  async.waterfall([
-    function(next) {
-      var request_url = api_url+"orders.get&my&id="+api_id+"&key="+api_key+"&location=0&algo=1";
-      httpsRequest(request_url,next);
-    },
-    function(result,next) {
-      order_id = result.result.orders[0].id;
-      var request_url = api_url+"orders.set.price.decrease&id="+api_id+"&key="+api_key+"&location=0&algo=1&order="+order_id;
-      httpsRequest(request_url,next);
-    }],
-    function(err,result) {
-      var replySpeech = "";
+
+  getOrderInfo(function(err,orders) {
+    if (err) {
+      replySpeech = "There was a system error getting your order info.";
+      callback(replySpeech,true);
+    }
+
+// No support for prompting when multiple orders exist, so for now just going to modify the first order
+    var order_id = orders[0].id;
+    var order_location = orders[0].location;
+    var request_url = api_url+"orders.set.price.decrease&id="+api_id+"&key="+api_key+"&location="+order_location+"&algo=1&order="+order_id; 
+    httpsRequest(request_url,function(err,response) {
       if (err) {
         replySpeech = "There was a system error while attempting to lower your price.";
 	console.log("Error: "+err);
       } else {
-        if (result.result.success) {
-          replySpeech = "Order "+order_id+" price has been lowered. "+result.result.success;
-	} else if (result.result.error) {
-	  replySpeech = "Order "+order_id+" price could not be changed at this time. "+result.result.error;
+        if (response.result.success) {
+          replySpeech = "Order "+order_id+" price has been lowered. "+response.result.success;
+	} else if (response.result.error) {
+          replySpeech = "Order "+order_id+" price could not be changed at this time. "+response.result.error;
 	} else {
 	  replySpeech = "Order "+order_id+" price could not be changed at this time.";
 	  console.log("Result: "+result);
 	}
       }
-
       callback(replySpeech,true);
     });
+  });
 }
 
 function doRaisePrice(intent,callback) {
-  var order_id="";
 
-  async.waterfall([
-    function(next) {
-      var request_url = api_url+"orders.get&my&id="+api_id+"&key="+api_key+"&location=0&algo=1";
-      httpsRequest(request_url,next);
-    },
-    function(result,next) {
-      order_id = result.result.orders[0].id;
-      var old_price = result.result.orders[0].price;
-      var new_price = Number(old_price) + Number(0.0001);
-      console.log("Price increase from "+old_price+" to "+new_price);
-      var request_url = api_url+"orders.set.price&id="+api_id+"&key="+api_key+"&location=0&algo=1&order="+order_id+"&price="+new_price;
-      httpsRequest(request_url,next);
-    }],
-    function(err,result) {
-      var replySpeech = "";
+  getOrderInfo(function(err,orders) {
+    if (err) {
+      replySpeech = "There was a system error getting your order info.";
+      callback(replySpeech,true);
+    }
+
+// No support for prompting when multiple orders exist, so for now just going to modify the first order
+    var order_id = orders[0].id;
+    var order_location = orders[0].location;
+    var old_price = orders[0].price;
+    var new_price = Number(old_price) + Number(0.0001);
+    console.log("Price increase from "+old_price+" to "+new_price);
+    var request_url = api_url+"orders.set.price&id="+api_id+"&key="+api_key+"&location="+order_location+"&algo=1&order="+order_id+"&price="+new_price;
+    httpsRequest(request_url,function(err,response) {
       if (err) {
         replySpeech = "There was a system error while attempting to raise your price.";
         console.log("Error: "+err);
       } else {
-        if (result.result.success) {
-          replySpeech = "Order "+order_id+" price has been raised. "+result.result.success;
-        } else if (result.result.error) {
-          replySpeech = "Order "+order_id+" price could not be changed at this time. "+result.result.error;
+        if (response.result.success) {
+          replySpeech = "Order "+order_id+" price has been raised. "+response.result.success;
+        } else if (response.result.error) {
+          replySpeech = "Order "+order_id+" price could not be changed at this time. "+response.result.error;
         } else {
           replySpeech = "Order "+order_id+" price could not be changed at this time.";
           console.log("Result: "+result);
         }
       }
-
       callback(replySpeech,true);
     });
+  });
 }
 
 
