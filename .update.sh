ce4047ec03f5e1dcb8a4a20ac0065e7249dc4ce9
Brian Sneddon <bsneddon@gc1.com>
2016-05-14 16:33:32 -0400

    Removing node_modules from repo and replacing with package.json definition

--- .update.sh
+++ .update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+npm install
 zip -r AlexaNiceHash.zip *
 aws --profile personal lambda update-function-code --zip-file fileb://AlexaNiceHash.zip --function-name AlexaNiceHash --publish --region us-east-1
 rm AlexaNiceHash.zip
